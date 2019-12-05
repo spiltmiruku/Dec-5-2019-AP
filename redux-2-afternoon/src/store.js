2b2ac0a7da12e0f338962daded58575005340242
spiltmiruku <spiltmiruku@gmail.com>
2019-12-05 14:41:29 -0700

    Initial commit

---
+++ redux-2-afternoon/src/store.js
@@ -0,0 +1,10 @@
+import {createStore, combineReducers, applyMiddleware} from 'redux';
+import promiseMiddleware from 'redux-promise-middleware';
+import budgetReducer from './ducks/budgetReducer';
+
+const rootReducer = combineReducers({
+   budget: budgetReducer,
+
+});
+
+export default createStore(rootReducer, applyMiddleware(promiseMiddleware));
